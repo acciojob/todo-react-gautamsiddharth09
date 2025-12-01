b6d3af5db9de45742e5b1745d263d11408723237
Gautam kumar <gautamsiddharth2013@gmail.com>
2025-12-01 16:10:54 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 
 import React from "react";
 import './../styles/App.css';
+import ToDoList from "./ToDoList.";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+         <ToDoList />
     </div>
   )
 }
 
 export default App
+
